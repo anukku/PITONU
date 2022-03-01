f8133ac2c8455e2e3c5745e74acc625724236500
anukku <69959462+anukku@users.noreply.github.com>
2022-03-01 21:14:20 +0200

    Add files via upload

--- Python/syntax_dictionaries_1.py
+++ Python/syntax_dictionaries_1.py
@@ -25,4 +25,21 @@ employee_info["Hobbies"] = "Playing video games", "Reading"
 if "Favorite Color" in employee_info:
     print(employee_info["Favorite Color"])
 
-print(employee_info.get("Favorite Color"))    
+print(employee_info.get("Favorite Color"))
+
+employee_info_1 = {
+    "Name": "Will",
+    "Last Name": "Oldman",
+    "Address": "New York, USA",
+    "Job": {"Salesperson", "Developer"},
+    "Age": 34,
+    "Hobbies": ("Reading", "Wacthing Movies", "Walking"),
+    1992: "Date of Birth",
+    "Married": True,
+    "Favorite Songs": ["Un Dia", "Blinding Lights", "Shallow"],
+    "Best Friend": {"Name": "Gerald"},
+    "Special One": None
+}
+
+employee_info_1.clear()
+print(employee_info_1)
---
+++ Python/syntax_dictionaries_2.py
@@ -0,0 +1,13 @@
+employee_info_1 = {
+    "Name": "Will",
+    "Last Name": "Oldman",
+    "Address": "New York, USA",
+    "Job": {"Salesperson", "Developer"},
+    "Age": 34,
+    "Hobbies": ("Reading", "Wacthing Movies", "Walking"),
+    1992: "Date of Birth",
+    "Married": True,
+    "Favorite Songs": ["Un Dia", "Blinding Lights", "Shallow"],
+    "Best Friend": {"Name": "Gerald"},
+    "Special One": None
+}
